2bce36b0213d52407444bc03ce8a83721e3e7ed4
Andrés Nuñez <andresanunez98@gmail.com>
2021-05-28 17:16:02 -0400

    commit tree 997a69c59e2ba9ab9dd9c6b5b00f3a2aa3e20cb8

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function superbowlWin(record){ 
+   let found = record.find(property =>{return property.result === "W"} )
+    if (found){
+        return found.year
+    } else {
+        return undefined
+    }
 
+}
